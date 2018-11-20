aa83675d0e01da91bf289f770b212fb7600c2ffc
zcast7172 <44588356+zcast7172@users.noreply.github.com>
2018-11-20 13:26:32 -0500

    Add files via upload

---
+++ SeniorDesign.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# -*- coding: utf-8 -*-
+#############################
+#   Capstone Project 2018
+#############################
+#   twitter
+#   username: opinionpsu
+#   password: senior123
+#############################
+#   authors: 
+#   Nick Brougher, Zechariah Castillo, Taylor Novotny, Nikos Koufos
+#############################
+
+import praw # Reddit API, you might have to pip install on first use and restart ide
+import pandas as pd
+import requests
+import urllib
+import json
+import twitter
+import unittest
+#New Imports
+################################
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+import re,string
+
+################################
+from rake_nltk import Rake
+
+
+# Twitter keys
+tw_consumer_key = "NUw4vNInQbQwuJxpUWqwpH9Qf"
+tw_consumer_secret = "mICLV2cokUoTqPRJokrYftJPnqqD0lcdNSDizVf5K9IWZVVVzp"
+tw_access_token = "1044098405780402176-2nNytlbAYFllXj9lfvLwI9hc98WgAA"
+tw_access_token_secret = "noRjMgV5o4x5bvw9Azj3E45BQ3wT8lcwllUNS4vawUy8Q"
+
+# Reddit Keys
+rd_client_id = 'mnXq68SWnf4wZA'
+rd_client_secret = 'KMm4PQBfhMlHnV4Tu-fEVsrfXG4'
+rd_user_agent = 'capstone123'
+
+
+def strip_links(text):
+    link_regex = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
+    links = re.findall(link_regex, text)
+    for link in links:
+        text = text.replace(link[0], ', ')    
+    return text
+
+def strip_all_entities(text):
+    entity_prefixes = ['@','#']
+    for separator in  string.punctuation:
+        if separator not in entity_prefixes :
+            text = text.replace(separator,' ')
+    words = []
+    for word in text.split():
+        word = word.strip()
+        if word:
+            if word[0] not in entity_prefixes:
+                words.append(word)
+    return ' '.join(words)
+
+def initProgram():
+    re = Reddit()
+    ke = KeywordExtraction(re)
+    Twitter(ke)
+    
+# ~ 'Reddit' Returns DataFrame of articles ~ #
+def Reddit():
+    reddit = praw.Reddit(client_id=rd_client_id, client_secret=rd_client_secret, user_agent=rd_user_agent)
+    subreddit = reddit.subreddit('news') #creates object representing subreddit
+    hot_articles = subreddit.hot(limit=1) #gets list of hot submissions in subreddit
+    article_df = pd.DataFrame() # DataFrame of articles
+    for submission in hot_articles: #iterates through submissions, adds titles to dataframe
+        article_df.at[len(article_df), 'Title'] = submission.title
+    
+    print("{ Article DataFrame }\n", article_df, "\n")
+    return article_df
+# ~ Reddit() ~ #
+   
+# ~ 'KeywordExtraction' Extracts N keywords from N articles ~ #
+def KeywordExtraction(article_df):
+    ra = Rake()
+    keyword_df = pd.DataFrame()
+    for row in article_df.itertuples(index=True, name='Pandas'):
+        article = getattr(row, 'Title')
+        print("Performing Keyword Extraction on this article: \n[", article, "]\n")
+        keyword_df = ra.extract_keywords_from_text(article)
+        print(keyword_df)
+        print("Ranked Phrases With Scores: ", ra.get_ranked_phrases_with_scores(), "\n")
+        keyword_df.at[len(keyword_df), row] = keyword_df #error here
+        print("--------------------------------")
+    return keyword_df
+# ~ KeywordExtraction() ~ #
+    
+# ~ 'Twitter' Returns DataFrame of tweets related to Keyword ~ #
+def Twitter(keyword_df):
+    api = twitter.api.Api(consumer_key=tw_consumer_key,
+                  consumer_secret=tw_consumer_secret,
+                  access_token_key=tw_access_token,
+                  access_token_secret=tw_access_token_secret)
+    #print(api.VerifyCredentials())
+    count=0
+    for keyword in keyword_df:
+        print(keyword)
+        #keyword = "Canada" #pass in keyword dataframe. In a forloop
+        query = "q=" + keyword + "&result_type=popular&count=10&tweet_mode=extended"
+        results = api.GetSearch(raw_query=query)
+        delimitedText = strip_all_entities(strip_links(results[count].full_text))
+        print(delimitedText)
+        count=count+1
+# ~ Twitter() ~ #
+    
+# ~ 'SentimentAnalysis' Performs Sentiment Analysis on Tweets ~ #
+def SentimentAnalysis():
+    print("Hello World")
+# ~ SentimentAnalysis ~ #
+    
+# ~ 'PublicOpinion' Returns Public Opinion Based on Sentiment Analysis ~ #
+def PublicOpinion():
+    print("Hello World")
+# ~ PublicOpinion ~ #
+
+initProgram()
+
